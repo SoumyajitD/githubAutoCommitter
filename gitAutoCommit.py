82c08cf61843df95a43e4094aef84c94c5c3be27
Soumyajit Dawn <soumyajitdawn@gmail.com>
2021-09-08 21:38:39 +0530

    Delete gitAutoCommit.py

--- gitAutoCommit.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-def makeCommit():
-    os.system('git add .')
-    os.system('git commit -m "Auto Commit by github auto commiter"')
-    time.sleep(3600)
-
-while True:    
-    makeCommit()
